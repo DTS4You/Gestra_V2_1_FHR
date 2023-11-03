e1ba16b5ede2f8d3ed1d958025aa4ffb32defe6a
Norbert.Schwarz <info@datentechnikschwarz.de>
2023-11-03 20:36:58 +0100

    Init

--- main_test.py
+++ main_test.py
@@ -9,26 +9,25 @@ import defaults
 
 loop_time = 0.5
 
-color = ( 0, 10, 0)
 
-
-class Colors:
-    default         = (  0,  0,  5)
-    start           = (  0, 20,  0)
-    end             = ( 20,  0,  0)
-    led_on          = ( 50, 50, 50)
-    led_off         = (  0,  0,  0)
-    target          = ( 10, 20, 30)
-    radar_send      = ( 10, 20, 30)
-    radar_receive   = ( 10, 20, 30)
-
-
-def set_pixel_ws2812(led_pio, pos, my_color=defaults.Colors.default):
-    r, g, b = my_color
-    lr = my_color[0]
-    lg = my_color[1]
-    lb = my_color[2]
-    print(my_color, " -> ", r, g, b, " -> ", lr, lg, lb, " -> ", len(my_color))
+hit_array = [
+            [ 0, 1],        #  1. Track
+            [ 2, 3],        #  2. Track
+            [ 4, 5],        #  3. Track
+            [ 0],           #  4. Track
+            [ 1],           #  5. Track
+            [ 0],           #  6. Track
+            [ 0],           #  7. Track
+            [ 0],           #  8. Track
+            [ 0],           #  9. Track
+            [ 0],           # 10. Track
+            [ 0],           # 11. Track
+            [ 0],           # 12. Track
+            [ 0],           # 13. Track
+            [ 0],           # 14. Track
+            [ 0],           # 15. Track
+            [ 0]            # 16. Track
+]
 
 
 # -----------------------------------------------------------------------------
@@ -36,19 +35,19 @@ def main_loop():
 
     print("Main Loop")
 
-    my_color = Colors()
+    rows = len(hit_array)
+    print(rows)
+    for elements in hit_array[4]:
+        print(elements)
 
-    i = 0
-
-    while i < 100:
-        set_pixel_ws2812(0, 0, my_color.start)
-        time.sleep(loop_time)
-        i += 1
-
+    print("Main End")
 
 # =============================================================================
+
+
 if __name__ == '__main__':
 
     main_loop()
     print("=== End of Program ===")
+
 # =============================================================================
